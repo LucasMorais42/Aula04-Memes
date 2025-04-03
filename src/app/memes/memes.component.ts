c76ccead2e8f0e204354d3e757afbd38dfa8186a
LucasMorais42 <lucasmorais2214@gmail.com>
2025-04-03 17:54:03 -0300

    Envio projeto parcial

---
+++ src/app/memes/memes.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-memes',
+  standalone: false,
+  templateUrl: './memes.component.html',
+  styleUrl: './memes.component.css'
+})
+export class MemesComponent {
+
+}
